6b87309234519a596233459b14d4824d5d767c56
Mark Bodis <75426536+kozaabOi@users.noreply.github.com>
2023-10-12 10:39:32 +0200

    map, filter

--- script.js
+++ script.js
@@ -1,25 +1,69 @@
-/*function getFirstChars(str, n) {
-  let result = ""
+const arr = ["kutya", "cica", "mérési hiba"]
 
-  result = str.substring(0, n + 1)
+const numbers = [10, 20, 30, 40, 50]
 
-  return result
-}
+/*for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i])
+}*/
 
-console.log(getFirstChars("Codecool", 5));*/
+/*for (const element of arr) {
+  console.log(element)
+}*/
 
+//arr.forEach((element) => console.log(element))
 
-/*let text = "lorem ipsum dolor et"
+/*const newNumbers = []
 
-console.log(text.substring(6, 11))
+numbers.forEach(function (number, index, array) {
+  let newNumber = number * 2
+  console.log(newNumber, index, array)
+  newNumbers.push(newNumber)
+})
 
-const obj = {
-  firstName: "Mark",
-  lastName: "Bodis",
-  getFullName: function() {
-    return obj.firstName + obj.lastName
+console.log(newNumbers)*/
+
+/*const newNumbers = []
+
+numbers.map(function(number, index, array) {
+  let newNumber = number * 2
+  console.log(newNumber, index, array)
+  newNumbers.push(newNumber)
+  return newNumber
+})*/
+
+const mapResult = numbers.map((number) => number * 2)
+
+/*console.log(numbers)
+console.log(mapResult)*/
+
+const numberArray = [6, 11, 45, 8, 10, 16, 100]
+
+const users = [
+  {
+    name: "Márk",
+    age: 28,
+    budget: 20000
+  },
+  {
+    name: "Lajos",
+    age: 22,
+    budget: 1200
+  },
+  {
+    name: "Olivér",
+    age: 22,
+    budget:999999
+  },
+  {
+    name: "Örs",
+    age: 44,
+    budget: 124000
   }
-}
+]
 
-console.log(obj.getFullName())*/
+const filterResult = numberArray.filter((number) => number % 2 !== 0)
 
+//console.log(filterResult)
+
+const usersWithHighBudget = users.filter(user => user.budget > 20000)
+console.log(usersWithHighBudget)
